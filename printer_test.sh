ce7a5751f224fce162a1016b3d8bd08cb5a3275c
J Lincho <41831414+Jolium@users.noreply.github.com>
2022-11-09 19:33:59 +0100

    Update printer_test.sh

--- printer_test.sh
+++ printer_test.sh
@@ -2,11 +2,3 @@
 
 cd || { echo "cd failed"; exit 1; }
 lp test_page_printer.pdf
-
-
-# Open crontab
-# >> crontab -e
-
-# Add to the end of the file
-# # Runs every saturday at 14:00 (UTC +1)
-# 0 13 * * 6 /home/<username>/printer_test.sh
